8df5ad1294dfda700c4856d39dc9af87b5cf0878
Jason Crawford <daxapps777@gmail.com>
2019-05-29 20:46:35 -0400

    business logic

--- App.js
+++ App.js
@@ -1,11 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Button } from 'native-base';
+import { FontAwesome } from '@expo/vector-icons';
+
+var itemArray = new Array(25).fill('empty');
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      randomNumber: ""
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ randomNumber: this.generateRandomNumber() });
+  };
+
+  generateRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random() * 25);
+    this.setState({ randomNumber: randomNumber, isScratched: true });
+    return randomNumber;
+  };
+
+  scratchItem = itemNumber => {
+    if (this.state.randomNumber === itemNumber) {
+      itemArray[itemNumber] = "lucky";
+    } else {
+      itemArray[itemNumber] = "unlucky";
+    }
+    this.forceUpdate();
+  };
+
+  scratchItemIcon = itemNumber => {
+    if (itemArray[itemNumber] === "lucky") {
+      return "dollar"
+    } else if (itemArray[itemNumber] === "unlucky") {
+      return "frown-o"
+    }
+    return "circle"
+  };
+
+  scratchItenColor = () => {
+    if (itemArray[itemNumber] === "lucky") {
+      return "green"
+    } else if (itemArray[itemNumber] === "unlucky") {
+      return "red"
+    }
+    return "black"
+  }
+
+  showAllItem = () => {
+    itemArray.fill('unlucky');
+    itemArray[this.state.randomNumber] = 'lucky'
+
+    this.forceUpdate();
+  }
+
+  resetGame = () => {
+    this.setState({randomNumber: this.generateRandomNumber()}, () => {
+      itemArray.fill('empty');
+      this.forceUpdate;
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>DaxApps!</Text>
       </View>
     );
   }
